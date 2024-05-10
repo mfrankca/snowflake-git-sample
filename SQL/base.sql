40ae190a1a8d2f01345e79cc6d6cde18b99792c5
Marina frank <mfrankca@yahoo.ca>
2024-05-10 00:10:26 -0400

    change

--- SQL/base.sql
+++ SQL/base.sql
@@ -1,3 +1,3 @@
-EXECUTE IMMEDIATE FROM './tables/test.sql';
+EXECUTE IMMEDIATE FROM './tables/test_table.sql';
 EXECUTE IMMEDIATE FROM './procedures/test_procedure.sql';
 EXECUTE IMMEDIATE FROM './tasks/test_task.sql';

fa6de97b05d987fee63a6cc297255aa967a75892
Marina frank <mfrankca@yahoo.ca>
2024-05-09 23:47:23 -0400

    updated

--- SQL/base.sql
+++ SQL/base.sql
@@ -1,3 +1,3 @@
-EXECUTE IMMEDIATE FROM './tables/menu.sql';
-EXECUTE IMMEDIATE FROM './procedures/menu_procedure.sql';
-EXECUTE IMMEDIATE FROM './tasks/menu_task.sql';
+EXECUTE IMMEDIATE FROM './tables/test.sql';
+EXECUTE IMMEDIATE FROM './procedures/test_procedure.sql';
+EXECUTE IMMEDIATE FROM './tasks/test_task.sql';
